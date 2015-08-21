8a371bd876b0d066db0a04e9151b33f0f4f41e01
alice8347 <alice8347@hotmail.com>
2015-08-20 23:14:19 -0400

    Second Commit
    Add all search functions

--- ProStrongheimGradebook/src/Request.java
+++ ProStrongheimGradebook/src/Request.java
@@ -51,17 +51,120 @@ public class Request extends HttpServlet {
 			type = request.getParameter("type");
 		}
 		
-		String tBody = getSelectedTable(stuId, type);
 		if (request.getParameter("allAssi") != null) {
+			String tBody = getSelectedTable(stuId, type);
 			content += "<table class=\"table table-striped\"><thead><tr><th>id</th><th>Student ID</th><th>Assignment Name</th><th>Type</th><th>Date</th><th>Grade</th></tr></thead><tbody>"
 					+ tBody + "</tbody></table>";
+		}
+		
+		if (request.getParameter("average") != null) {
+			String ave = getAve(stuId, type);
+			content += "<div class=\"jumbotron\"><h4>Average grade: </h4><p>" + ave + "</p></div>";
+		}
+		
+		if (request.getParameter("maxMin") != null) {
+			String value[] = getMaxMin(stuId, type);
+			content += "<div class=\"jumbotron\"><h4>Highest grade: </h4><p>" + value[0] + "</p><br /><h4>Lowest grade: </h4><p>" + value[1] + "</p></div>";
 		}
 		request.setAttribute("content", content);
 		getServletContext().getRequestDispatcher("/RequestOutput.jsp").forward(request, response);
 	}
-	
+
 	private String getSelectedTable(String stuId, String type) {
 		String content = "";
+		Connection conn = connectDB();
+
+		String sql = "";
+		if ((stuId.isEmpty() == true) && (type.isEmpty() == true)) {
+			sql = "SELECT * FROM grades";
+		} else if ((stuId.isEmpty() == false) && (type.isEmpty() == true)) {
+			sql = "SELECT * FROM grades WHERE stu_id = '" + stuId + "'";
+		} else if ((stuId.isEmpty() == true) && (type.isEmpty() == false)) {
+			sql = "SELECT * FROM grades WHERE type = '" + type + "'";
+		} else {
+			sql = "SELECT * FROM grades WHERE stu_id = '" + stuId + "' AND type = '" + type + "'";
+		}
+
+		// creating PreparedStatement object to execute query
+		PreparedStatement preStatement = conn.prepareStatement(sql);
+		ResultSet result = preStatement.executeQuery();
+
+		while (result.next()) {
+			content += "<tr><td>"
+					+ Integer.parseInt(result.getString("id"))
+					+ "</td><td>" 
+					+ result.getString("stu_id")
+					+ "</td><td>"
+					+ result.getString("assignment")
+					+ "</td><td>"
+					+ result.getString("type")
+					+ "</td><td>"
+					+ result.getString("assi_date").substring(0, 10)
+					+ "</td><td>"
+					+ Double.parseDouble(result.getString("grade"))
+					+ "</td></tr>";
+		}
+		return content;
+	}
+	
+	private String getAve(String stuId, String type) {
+		String content = "";
+		Connection conn = connectDB();
+
+		String sql = "";
+		if ((stuId.isEmpty() == true) && (type.isEmpty() == true)) {
+			sql = "SELECT AVG(grade) FROM grades";
+		} else if ((stuId.isEmpty() == false) && (type.isEmpty() == true)) {
+			sql = "SELECT AVG(grade) FROM grades WHERE stu_id = '" + stuId + "'";
+		} else if ((stuId.isEmpty() == true) && (type.isEmpty() == false)) {
+			sql = "SELECT AVG(grade) FROM grades WHERE type = '" + type + "'";
+		} else {
+			sql = "SELECT AVG(grade) FROM grades WHERE stu_id = '" + stuId + "' AND type = '" + type + "'";
+		}
+
+		// creating PreparedStatement object to execute query
+		PreparedStatement preStatement = conn.prepareStatement(sql);
+		ResultSet result = preStatement.executeQuery();
+		
+		while (result.next()) {
+			content += result.getString(1);
+		}
+		return content;
+	}
+	
+	private String[] getMaxMin() {
+		String[] content = new String[2];
+		Connection conn = connectDB();
+		
+		String sql1 = "", sql2 = "";
+		if ((stuId.isEmpty() == true) && (type.isEmpty() == true)) {
+			sql1 = "SELECT MAX(grade) FROM grades";
+			sql2 = "SELECT MIN(grade) FROM grades";
+		} else if ((stuId.isEmpty() == false) && (type.isEmpty() == true)) {
+			sql1 = "SELECT MAX(grade) FROM grades WHERE stu_id = '" + stuId + "'";
+			sql2 = "SELECT MIN(grade) FROM grades WHERE stu_id = '" + stuId + "'";
+		} else if ((stuId.isEmpty() == true) && (type.isEmpty() == false)) {
+			sql1 = "SELECT MAX(grade) FROM grades WHERE type = '" + type + "'";
+			sql2 = "SELECT MIN(grade) FROM grades WHERE type = '" + type + "'";
+		} else {
+			sql1 = "SELECT MAX(grade) FROM grades WHERE stu_id = '" + stuId + "' AND type = '" + type + "'";
+			sql2 = "SELECT MIN(grade) FROM grades WHERE stu_id = '" + stuId + "' AND type = '" + type + "'";
+		}
+
+		// creating PreparedStatement object to execute query
+		PreparedStatement preStatement1 = conn.prepareStatement(sql1);
+		PreparedStatement preStatement2 = conn.prepareStatement(sql2);
+		ResultSet result1 = preStatement1.executeQuery();
+		ResultSet result2 = preStatement2.executeQuery();
+		
+		while (result1.next() && result2.next()) {
+			content[0] += result1.getString(1);
+			content[1] += result2.getString(1);
+		}
+		return content;
+	}
+
+	private static Connection connectDB() {
 		try {
 			// URL of Oracle database server
 			String url = "jdbc:oracle:thin:testuser/password@localhost";
@@ -74,43 +177,12 @@ public class Request extends HttpServlet {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			// creating connection to Oracle database using JDBC
 			Connection conn = DriverManager.getConnection(url, props);
-
-			String sql = "";
-			if ((stuId.isEmpty() == true) && (type.isEmpty() == true)) {
-				sql = "SELECT * FROM grades";
-			} else if ((stuId.isEmpty() == false) && (type.isEmpty() == true)) {
-				sql = "SELECT * FROM grades WHERE stu_id = '" + stuId + "'";
-			} else if ((stuId.isEmpty() == true) && (type.isEmpty() == false)) {
-				sql = "SELECT * FROM grades WHERE type = '" + type + "'";
-			} else {
-				sql = "SELECT * FROM grades WHERE stu_id = '" + stuId + "' AND type = '" + type + "'";
-			}
-
-			// creating PreparedStatement object to execute query
-			PreparedStatement preStatement = conn.prepareStatement(sql);
-			ResultSet result = preStatement.executeQuery();
-
-			while (result.next()) {
-				content += "<tr><td>"
-						+ Integer.parseInt(result.getString("id"))
-						+ "</td><td>" 
-						+ result.getString("stu_id")
-						+ "</td><td>"
-						+ result.getString("assignment")
-						+ "</td><td>"
-						+ result.getString("type")
-						+ "</td><td>"
-						+ result.getString("assi_date").substring(0, 10)
-						+ "</td><td>"
-						+ Double.parseDouble(result.getString("grade"))
-						+ "</td></tr>";
-			}
 		} catch (SQLException e) {
 			e.getMessage();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		return content;
+		return conn;
 	}
-
+	
 }
